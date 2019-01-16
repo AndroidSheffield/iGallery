572832ecb803608aca8105c9d2a3796bce147da8
Batawar <linjingbo95@gmail.com>
2019-01-16 17:19:42 +0000

    upgrade main ui

--- app/src/main/java/com/nexus/igallery/views/ShowImageActivity.java
+++ app/src/main/java/com/nexus/igallery/views/ShowImageActivity.java
@@ -1,7 +1,3 @@
-/*
- * Copyright (c) 2017. This code has been developed by Fabio Ciravegna, The University of Sheffield. All rights reserved. No part of this code can be used without the explicit written permission by the author
- */
-
 package com.nexus.igallery.views;
 
 import android.content.Intent;
